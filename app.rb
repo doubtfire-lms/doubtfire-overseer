246fc597003d0c1fa1ed78e3931da68d81eddf48
Andrew Cain <macite@gmail.com>
2021-07-19 11:40:28 +1000

    new: Simplify overseer configuration

--- app.rb
+++ app.rb
@@ -9,25 +9,26 @@ subscriber_config = {
   RABBITMQ_HOSTNAME: ENV['RABBITMQ_HOSTNAME'],
   RABBITMQ_USERNAME: ENV['RABBITMQ_USERNAME'],
   RABBITMQ_PASSWORD: ENV['RABBITMQ_PASSWORD'],
-  EXCHANGE_NAME: ENV['EXCHANGE_NAME'],
-  DURABLE_QUEUE_NAME: ENV['DURABLE_QUEUE_NAME'],
-  BINDING_KEYS: ENV['BINDING_KEYS'],
-  DEFAULT_BINDING_KEY: ENV['DEFAULT_BINDING_KEY']
+  EXCHANGE_NAME: 'ontrack',
+  DURABLE_QUEUE_NAME: 'q.tasks',
+  BINDING_KEYS: 'task.submission',
+  DEFAULT_BINDING_KEY: 'task.submission'
 }
 
 assessment_results_publisher_config = {
   RABBITMQ_HOSTNAME: ENV['RABBITMQ_HOSTNAME'],
   RABBITMQ_USERNAME: ENV['RABBITMQ_USERNAME'],
   RABBITMQ_PASSWORD: ENV['RABBITMQ_PASSWORD'],
-  EXCHANGE_NAME: ENV['EXCHANGE_NAME'],
-  DURABLE_QUEUE_NAME: 'assessment_results',
+  EXCHANGE_NAME: 'ontrack',
+  DURABLE_QUEUE_NAME: 'q.overseer',
   # Publisher specific key
   # Note: `*.result` works too, but it makes no sense using that.
-  ROUTING_KEY: 'assessment.result'
+  ROUTING_KEY: 'overseer.result'
 }
 
 assessment_results_publisher = Publisher.new assessment_results_publisher_config
 
+# Register subscriber for task submissions, runs overseer receive, and publishes results to assessment_results_publisher
 register_subscriber(subscriber_config,
                     method(:receive),
                     assessment_results_publisher)
